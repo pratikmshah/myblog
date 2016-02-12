eddbcc10e825536b3432bb820d4e03e8dfe18808
Pratik Shah <pratik.t.shah@gmail.com>
2016-02-12 16:02:46 -0600

    Add association between post and comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
 
   root 'posts#index'   # redirect root page to posts index
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
 end
