6999cf0383a0594f47b527f791f9e4c1b7a95679
Pratik Shah <pratik.t.shah@gmail.com>
2016-02-12 15:28:00 -0600

    Add delete of post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,6 +37,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private #========= PRIVATE
