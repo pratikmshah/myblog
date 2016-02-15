cf9e81da2a6308c2abe3d2613fdaff14b4fb9aa9
Pratik Shah <pratik.t.shah@gmail.com>
2016-02-15 11:27:31 -0600

    Toggle text on comment link/button

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,13 +1,21 @@
 /* All related comment actions will be managed here */
 $(function(){
 
-  $("#comment-form").toggle();
+  toggleLink('#comment-form');  // initially hide comment form
 
-  // show form when user clicks on comment
-  $("h2").on('click', '#view-comment-form', function(e){
+  // show/hide form when user clicks on comment
+  $('h3').on('click', '#view-comment-form', function(e){
     e.preventDefault();
-    toggleLink("#comment-form");
+    toggleLink('#comment-form');
   });
+
+  // edit form text when user clicks on comment
+  $('h3').on('click', '#view-comment-form', function(e){
+    e.preventDefault();
+    linkText = $(this);
+    linkText.text(toggleLinkText(linkText));  // set text depending on current text
+  });
+
 
 });
 
@@ -16,4 +24,11 @@ $(function(){
 // toggle between show and hidden
 function toggleLink(selector) {
   $(selector).toggle();
+}
+
+// toggle text on link/button
+function toggleLinkText(linkText) {
+  text = linkText.text();
+  var text = text === "Add a comment" ? "Cancel comment" : "Add a comment";
+  return text;
 }
