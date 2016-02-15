a52cbaf22eff3896840b718db4a7b218c2f0b4da
Pratik Shah <pratik.t.shah@gmail.com>
2016-02-15 14:24:09 -0600

    Collapse comment form after user submits

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,24 +3,35 @@
 
 $(function(){
 
-  toggleLink('#comment_form');  // initially hide comment form
+  // declare variables for link, button, form ids
+  var viewCommentForm = '#view_comment_form';
+  var commentForm = '#comment_form';
+  var newComment = '#new_comment';
+
+  toggleLink(commentForm);  // initially hide comment form
 
   // show/hide form when user clicks on comment
-  $('h3').on('click', '#view_comment_form', function(e){
+  $('h3').on('click', viewCommentForm, function(e) {
     e.preventDefault();
-    toggleLink('#comment_form');
+    toggleLink(commentForm);
   });
 
   // edit form text when user clicks on comment
-  $('h3').on('click', '#view_comment_form', function(e){
+  $('h3').on('click', viewCommentForm, function(e) {
     e.preventDefault();
-    linkText = $(this);
-    linkText.text(toggleLinkText(linkText));  // set text depending on current text
+    $(viewCommentForm).text(toggleLinkText($(viewCommentForm)));  // set text depending on current text
+  });
+
+  // remove comments form when user submits
+  $(commentForm).on('submit', newComment, function(e) {
+    e.preventDefault();
+    toggleLink(commentForm);
+    $(viewCommentForm).text(toggleLinkText($(viewCommentForm)));
   });
 
 });
 
-/* Helper Methods Below */
+/*** Helper Methods Below ***/
 
 // toggle between show and hidden
 function toggleLink(selector) {
