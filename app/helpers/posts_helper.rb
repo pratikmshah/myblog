bf8eb943fb4de99bf91fa73c8ea49d65086c4085
Pratik Shah <pratik.t.shah@gmail.com>
2016-02-16 14:41:48 -0600

    Add read full article button/design

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,8 @@
 module PostsHelper
+
  # extracts first paragraph of article and returns text
  def extract_paragraph(text)
-  return text.scan(/<p>(.*)<\/p>/)[0][0].html_safe
+  return text.scan(/<p>(.*)<\/p>/)[0][0].html_safe + '..'
  end
+
 end
