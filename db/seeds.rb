3c52885ff3191fc96e849462182367b36b20f526
Pratik Shah <pratik.t.shah@gmail.com>
2016-02-29 11:27:39 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
-User.create()
+# Create admin user
+User.create(username: "pratik", password: "pratik", admin: true)
+
+# create 15 posts
+user = User.first
+
+10.times do |x|
+  Post.create(title: Faker::Book.title, content: '<p>' + Faker::Hipster.paragraph(1 + rand(4)) + '</p>', user_id: user.id)
+end
